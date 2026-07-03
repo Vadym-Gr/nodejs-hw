3661c043bd0db0e9f077693f49f0765e16d44d91
Vadym-Gr <aivvfg@gmail.com>
2026-07-03 14:30:34 +0300

    patch

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -36,3 +36,19 @@ export const deleteNote = async (req, res) => {
 
   res.status(200).json(note);
 };
+
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findOneAndUpdate(
+    { _id: noteId }, // Шукаємо по id
+    req.body,
+    { returnDocument: "after" }, // повертаємо оновлений документ
+  );
+
+  if (!note) {
+	throw createHttpError(404, `Note with ID ${noteId} not found`);
+  }
+
+  res.status(200).json(note);
+};
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -3,6 +3,7 @@ import { getAllNotes } from '../controllers/notesController.js';
 import { getNoteById } from '../controllers/notesController.js';
 import { createNote } from '../controllers/notesController.js';
 import { deleteNote } from '../controllers/notesController.js';
+import { updateNote } from '../controllers/notesController.js';
 
 const notesRouter = Router();
 
@@ -17,5 +18,8 @@ notesRouter.post('/notes', createNote);
 
 // Видалити нотатку за ID
 notesRouter.delete('/notes/:noteId', deleteNote);
+
+// Оновити нотатку за ID
+notesRouter.patch('/notes/:noteId', updateNote);
 
 export default notesRouter;
