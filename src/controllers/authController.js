7a8bdfb5ebee564c4b35750e2b4e26439fb2d9cc
Vadym-Gr <aivvfg@gmail.com>
2026-07-25 01:20:58 +0300

    Stuur een e-mail v-03

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -140,3 +140,38 @@ export const requestResetEmail = async (req, res) => {
 		message: 'Password reset email sent successfully'
 	});
 };
+
+export const resetPassword = async (req, res) => {
+	const { token, password } = req.body;
+
+	// 1. Перевіряю/декодую токен
+  let payload;
+  try {
+    payload = jwt.verify(token, process.env.JWT_SECRET);
+  } catch {
+	  // Повертаю помилку якщо проблема при декодуванні
+		throw createHttpError(401, 'Invalid or expired token');
+  }
+
+  // 2. Шукаю користувача
+  const user = await User.findOne({  _id: payload.sub,  email: payload.email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+
+  // 3. Якщо користувач існує
+  // створюю новий пароль і оновлюю користувача
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await User.updateOne(
+	  { _id: user._id },
+	  { password: hashedPassword }
+  );
+
+  // 4. Інвалідую всі можливі попередні сесії користувача
+  await Session.deleteMany({ userId: user._id });
+
+	// 5. Повертаю успішну відповідь
+  res.status(200).json({
+    message: 'Password reset successfully. Please log in again.',
+  });
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,8 +6,12 @@ import { loginUser } from '../controllers/authController.js';
 import { loginUserSchema } from '../validations/authValidation.js';
 import { logoutUser } from '../controllers/authController.js';
 import { refreshUserSession } from '../controllers/authController.js';
+
 import { requestResetEmail } from '../controllers/authController.js';
 import { requestResetEmailSchema } from '../validations/authValidation.js';
+
+import { resetPassword } from '../controllers/authController.js';
+import { resetPasswordSchema } from '../validations/authValidation.js';
 
 const router = Router();
 
@@ -16,6 +20,7 @@ router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
 
-router.post(  '/auth/request-reset-email', celebrate(requestResetEmailSchema), requestResetEmail);
+router.post('/auth/request-reset-email', celebrate(requestResetEmailSchema), requestResetEmail);
+router.post('/auth/reset-password', celebrate(resetPasswordSchema), resetPassword);
 
 export default router;
