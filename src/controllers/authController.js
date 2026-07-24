1cc52ac7ebb28a8a61474b06b8a94e7a9b0582e9
Vadym-Gr <aivvfg@gmail.com>
2026-07-24 17:19:48 +0300

    Stuur een e-mail

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,6 +6,9 @@ import { Session } from '../models/session.js';
 import { createSession } from '../services/auth.js';
 import { setSessionCookies } from '../services/auth.js';
 
+import { sendEmail } from '../utils/sendEmail.js';
+import jwt from 'jsonwebtoken';
+
 export const registerUser = async (req, res) => {
 
   const existingUser = await User.findOne({ email: req.body.email });
@@ -102,6 +105,23 @@ export const requestResetEmail = async (req, res) => {
     return res.status(200).json({
       message: 'If this email exists, a reset link has been sent',
     });
+  }
+
+  const resetToken = jwt.sign(
+    { sub: user._id, email },
+    process.env.JWT_SECRET,
+    { expiresIn: '15m' },
+  );
+
+  try {
+    await sendEmail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: 'Reset your password',
+      html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+    });
+  } catch {
+    throw createHttpError(500, 'Failed to send the email, please try again later.');
   }
 
 	res.status(200).json({
